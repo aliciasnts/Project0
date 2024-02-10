0fd2969cf12bc6b895d26bb804ff0b0910f9e6f8
aliciasnts <vitoria.santos@dcomp.ufs.br>
2024-02-10 13:49:00 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Projeto1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("What book are you looking for?");
+        string usersSearching = Console.ReadLine();
+        Console.WriteLine("here is your book! :" + booksClassification);
+
+    }
+
+    public class booksClassification {
+        string name = "name of the book";
+        int publicationData = 0001;
+        string author = "name of the author";
+        int pages = 1000;
+    };
+
+
+
+
+}
